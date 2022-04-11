4dc11fab97d1deb1a8c28bce5e165f91557817ac
prathameshppai <prathameshppai@gmail.com>
2022-04-12 00:39:20 +0530

    Modified folder structure

--- Networking/main.tf
+++ Networking/main.tf
@@ -1,15 +1,12 @@
 terraform {
   backend "s3" {
     bucket = "terraform-storage-11042022"
-    key    = "vpc/terraform.tfstate"
-    region = "us-west-2"
+    key    = "Networking/terraform.tfstate"
+    region = var.region
   }
 }
 
 module "vpc" {
-  source    = "./modules"
+  source    = "../modules"
 }
 
-
-  
-  
