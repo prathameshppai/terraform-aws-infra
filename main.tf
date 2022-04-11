6edbdf34479349bd82842aef7c721e8a15d1af4e
prathameshppai <43901259+prathameshppai@users.noreply.github.com>
2022-04-11 23:39:12 +0530

    Added terraform backend

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-storage-11042022"
+    key    = "vpc/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
 module "vpc" {
   source    = "./modules"
 }
+
+
+  
+  
