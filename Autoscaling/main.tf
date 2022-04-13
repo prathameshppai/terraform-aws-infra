fd084a2931e1589e7915aba4b23b610ce67c1336
prathameshppai <43901259+prathameshppai@users.noreply.github.com>
2022-04-14 01:04:32 +0530

    Removed health check

--- Autoscaling/main.tf
+++ Autoscaling/main.tf
@@ -66,13 +66,13 @@ resource "aws_elb" "backend" {
     lb_protocol       = "http"
   }
 
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:8000/"
-    interval            = 30
-  }
+#   health_check {
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 3
+#     target              = "HTTP:8000/"
+#     interval            = 30
+#   }
 
   cross_zone_load_balancing   = true
   idle_timeout                = 400
