e4d181ca9c0ec4208b22b43adc54470db6704d5d
prathameshppai <prathameshppai@gmail.com>
2022-04-14 00:32:39 +0530

    Added output AZs

--- Networking/outputs.tf
+++ Networking/outputs.tf
@@ -6,3 +6,8 @@ output "private_subnets" {
   description = "List of IDs of private subnets"
   value = module.vpc.private_subnets
 }
+
+output "azs" {
+  description = "A list of availability zones specified as argument to this module"
+  value       = module.vpc.azs
+}
