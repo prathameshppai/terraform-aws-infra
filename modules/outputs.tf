5f817bd951c5482d4e51a2cb6cb2eb1e821e0e0e
prathameshppai <43901259+prathameshppai@users.noreply.github.com>
2022-04-14 00:28:05 +0530

    Update outputs.tf

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,3 +1,8 @@
 output "vpc_cidr_block" {
   value = module.vpc.vpc_cidr_block
 }
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value = module.vpc.private_subnets
+}
