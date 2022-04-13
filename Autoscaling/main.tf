31e954f169db6ea2ac4045b501f36fb604f4ca47
prathameshppai <43901259+prathameshppai@users.noreply.github.com>
2022-04-14 00:17:51 +0530

    Update main.tf

--- Autoscaling/main.tf
+++ Autoscaling/main.tf
@@ -16,7 +16,7 @@ data "terraform_remote_state" "Networking" {
 }
 
 resource "aws_launch_configuration" "backend" {
-  name_prefix   = "backend-"
+  name_prefix   = "${terraform.workspace}-backend-"
   image_id      = "ami-00ee4df451840fa9d" #Linux CentOS AMI
   instance_type = "t2.micro"
 
@@ -26,18 +26,18 @@ resource "aws_launch_configuration" "backend" {
 }
 
 resource "aws_autoscaling_group" "backend" {
-  name                 = "backend-asg"
+  name                 = "${terraform.workspace}-backend-asg"
   launch_configuration = aws_launch_configuration.backend.name
   min_size             = 1
   max_size             = 2
-  load_balancers       = aws_elb.backend
+  load_balancers       = [aws_elb.backend]
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_elb" "backend" {
-  name               = "backend-elb"
+  name               = "${terraform.workspace}-backend-elb"
   availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   listener {
@@ -45,14 +45,6 @@ resource "aws_elb" "backend" {
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
-  }
-
-  listener {
-    instance_port      = 8000
-    instance_protocol  = "http"
-    lb_port            = 443
-    lb_protocol        = "https"
-    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
   }
 
   health_check {
@@ -69,6 +61,6 @@ resource "aws_elb" "backend" {
   connection_draining_timeout = 400
 
   tags = {
-    Name = "foobar-terraform-elb"
+    Name = "${terraform.workspace}-elb"
   }
 }
