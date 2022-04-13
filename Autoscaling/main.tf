6159f2afa4341a61a4c88d5dff3b46ba48675a44
prathameshppai <43901259+prathameshppai@users.noreply.github.com>
2022-04-13 12:11:11 +0530

    Update main.tf

--- Autoscaling/main.tf
+++ Autoscaling/main.tf
@@ -10,7 +10,7 @@ data "terraform_remote_state" "Networking" {
   backend = "s3"
   config = {
     bucket = "terraform-storage-11042022"
-    key    = "Networking.tfstate"
+    key    = "env://${terraform.workspace}/Networking.tfstate"
     region = "us-west-2"
   }
 }
